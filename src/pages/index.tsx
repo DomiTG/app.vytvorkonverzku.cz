e5fa88ff722f16620ca3a0e417a2f28dc262bf09
Dominik Hůla <81680504+DomiTG@users.noreply.github.com>
2024-11-27 20:18:09 +0100

    test

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <div className="h-screen w-full flex flex-col justify-center items-center">
       <h1 className="text-4xl font-bold text-center uppercase">Konverzka</h1>
-      <p className="text-center">Konverzní stránky</p>
+      <p className="text-center">Konverzní stránky </p>
     </div>
   );
 }
